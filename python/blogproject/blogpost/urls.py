9a4f9b5ccc32a88ee5247d1f2cc6f441618ef237
ojipan <ojipan@ojipannoMacBook-Air.local>
2020-09-17 21:51:11 +0900

    bloghtml

--- python/blogproject/blogpost/urls.py
+++ python/blogproject/blogpost/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from.views import BlogList,BlogDetail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', BlogList.as_view()),
+    path('detail/<int:pk>/', BlogDetail.as_view()),
 ]

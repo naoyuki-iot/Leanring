8ae4da6c058a791b43b684818438bd0bfa21f4f7
naoyuki-iot <naoyuki1998513@gmail.com>
2020-12-18 16:41:20 +0900

    piyo

---
+++ python/csv/csvtest.py
@@ -0,0 +1,17 @@
+import csv
+from os import read, write
+import matplotlib.pyplot as plt
+import numpy as np
+
+input = "/Users/ojipan/Practice/python/csv/test.csv"
+output = "/Users/ojipan/Practice/python/csv/output.csv"
+data = []
+
+with open(input, mode = "r", newline = '') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
+
+with open(output, mode = "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(f)
